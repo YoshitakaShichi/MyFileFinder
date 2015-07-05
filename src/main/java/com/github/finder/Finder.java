c70f8ffe500c54197ecdf496f9ad8890f8de6bd9
Takeda <g1344793@cse.kyoto-su.ac.jp>
2015-07-05 16:34:21 +0900

    コマンドラインオプションで type オプションが指定された場合の処理を追加する。修正
    type が fもしくは file の場合は，ファイルであったとき isTargetメソッドは true を返す．
    type が dもしくは directory の場合は，ディレクトリであったとき isTargetメソッドは true を返す．
    type が hもしくは hidden の場合は，隠しファイルであったとき isTarget メソッドは true を返す．
    その他の場合は，falseを返す．

--- src/main/java/com/github/finder/Finder.java
+++ src/main/java/com/github/finder/Finder.java
@@ -6,43 +6,37 @@ import java.util.List;
 
 public class Finder {
     private Args args;
-
+    
     public Finder(Args args){
         this.args = args;
     }
-
+    
     public String[] find(String target){
         List<String> list = new ArrayList<>();
-
+        
         traverse(list, new File(target));
-
+        
         return list.toArray(new String[list.size()]);
     }
-
+    
     private void traverse(List<String> list, File dir){
         if(isTarget(dir)){
             list.add(dir.getPath());
         }
-
+        
         if(dir.isDirectory()){
             for(File file: dir.listFiles()){
                 traverse(list, file);
             }
         }
     }
-    
-    private boolean isTarget(File file){
-        boolean flag = true;
-        if(args.getName() != null){
-            flag &= checkTargetName(file, args.getName());
-        }
-        if(args.getType() != null){
-            flag &= checkTargetType(file, args.getType());
-        }
-        return flag;
+
+    private boolean checkTargetName(File file, String pattern){
+        String name = file.getName();
+        return name.indexOf(pattern) >= 0;
     }
     
-    private boolean checkTargetType(File file, String type){
+    private boolean checkTargetType(File file,String type){
         type = type.toLowerCase();
         if(type.equals("d") || type.equals("directory")){
             return file.isDirectory();
@@ -54,5 +48,16 @@ public class Finder {
             return file.isHidden();
         }
         return false;
+    }
+    
+    private boolean isTarget(File file){
+        boolean flag = true;
+        if(args.getName() != null){
+            flag &= checkTargetName(file, args.getName());
+        }
+        if(args.getType() != null){
+            flag &= checkTargetType(file, args.getType());
+        }
+        return flag;
     }
 }
